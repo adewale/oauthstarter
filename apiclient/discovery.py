66ce9e1d512c59ae106eea827a7913991db07207
Ade Oshineye <adewale@gmail.com>
2010-12-10 20:01:27 +0000

    Moved to latest version of Buzz wrapper. OAuth dance now works from localhost

--- apiclient/discovery.py
+++ apiclient/discovery.py
@@ -25,21 +25,40 @@ import httplib2
 import logging
 import os
 import re
-import simplejson
 import uritemplate
 import urllib
 import urlparse
+try:
+    from urlparse import parse_qsl
+except ImportError:
+    from cgi import parse_qsl
+from apiclient.http import HttpRequest
+from apiclient.json import simplejson
 
+URITEMPLATE = re.compile('{[^}]*}')
+VARNAME = re.compile('[a-zA-Z0-9_-]+')
 
-class HttpError(Exception):
+class Error(Exception):
+  """Base error for this module."""
   pass
 
 
-class UnknownLinkType(Exception):
+class HttpError(Error):
+  """HTTP data was invalid or unexpected."""
+  def __init__(self, resp, detail):
+    self.resp = resp
+    self.detail = detail
+  def __str__(self):
+    return self.detail
+
+
+class UnknownLinkType(Error):
+  """Link type unknown or unexpected."""
   pass
 
-DISCOVERY_URI = ('http://www.googleapis.com/discovery/0.1/describe'
-  '{?api,apiVersion}')
+
+DISCOVERY_URI = ('https://www.googleapis.com/discovery/v0.2beta1/describe/'
+  '{api}/{apiVersion}')
 
 
 def key2param(key):
@@ -64,15 +83,20 @@ class JsonModel(object):
   def request(self, headers, path_params, query_params, body_value):
     query = self.build_query(query_params)
     headers['accept'] = 'application/json'
+    if 'user-agent' in headers:
+      headers['user-agent'] += ' '
+    else:
+      headers['user-agent'] = ''
+    headers['user-agent'] += 'google-api-python-client/1.0'
     if body_value is None:
       return (headers, path_params, query, None)
     else:
-      model = {'data': body_value}
       headers['content-type'] = 'application/json'
-      return (headers, path_params, query, simplejson.dumps(model))
+      return (headers, path_params, query, simplejson.dumps(body_value))
 
   def build_query(self, params):
-    params.update({'alt': 'json', 'prettyprint': 'true'})
+    params.update({'alt': 'json'})
+    params.update({'pp': '1'})
     astuples = []
     for key, value in params.iteritems():
       if getattr(value, 'encode', False) and callable(value.encode):
@@ -84,36 +108,48 @@ class JsonModel(object):
     # Error handling is TBD, for example, do we retry
     # for some operation/error combinations?
     if resp.status < 300:
-      return simplejson.loads(content)['data']
+      if resp.status == 204:
+        # A 204: No Content response should be treated differently to all the other success states
+        return simplejson.loads('{}')
+      body = simplejson.loads(content)
+      if isinstance(body, dict) and 'data' in body:
+        body = body['data']
+      return body
     else:
       logging.debug('Content from bad request was: %s' % content)
-      if resp['content-type'] != 'application/json':
-        raise HttpError('%d %s' % (resp.status, resp.reason))
+      if resp.get('content-type', '').startswith('application/json'):
+        raise HttpError(resp, simplejson.loads(content)['error'])
       else:
-        raise HttpError(simplejson.loads(content)['error'])
+        raise HttpError(resp, '%d %s' % (resp.status, resp.reason))
 
 
-def build(serviceName, version, http=httplib2.Http(),
-    discoveryServiceUrl=DISCOVERY_URI, auth=None, model=JsonModel()):
+def build(serviceName, version, http=None,
+    discoveryServiceUrl=DISCOVERY_URI, developerKey=None, model=JsonModel()):
   params = {
       'api': serviceName,
       'apiVersion': version
       }
 
+  if http is None:
+    http = httplib2.Http()
   requested_url = uritemplate.expand(discoveryServiceUrl, params)
   logging.info('URL being requested: %s' % requested_url)
   resp, content = http.request(requested_url)
-  d = simplejson.loads(content)
-  service = d['data'][serviceName][version]
+  service = simplejson.loads(content)
 
   fn = os.path.join(os.path.dirname(__file__), "contrib",
       serviceName, "future.json")
-  f = file(fn, "r")
-  d = simplejson.load(f)
-  f.close()
-  future = d['data'][serviceName][version]['resources']
+  try:
+    f = file(fn, "r")
+    d = simplejson.load(f)
+    f.close()
+    future = d['resources']
+    auth_discovery = d['auth']
+  except IOError:
+    future = {}
+    auth_discovery = {}
 
-  base = service['baseUrl']
+  base = urlparse.urljoin(discoveryServiceUrl, service['restBasePath'])
   resources = service['resources']
 
   class Service(object):
@@ -123,23 +159,28 @@ def build(serviceName, version, http=httplib2.Http(),
       self._http = http
       self._baseUrl = base
       self._model = model
+      self._developerKey = developerKey
+
+    def auth_discovery(self):
+      return auth_discovery
 
   def createMethod(theclass, methodName, methodDesc, futureDesc):
 
-    def method(self, **kwargs):
+    def method(self):
       return createResource(self._http, self._baseUrl, self._model,
-          methodName, methodDesc, futureDesc)
+          methodName, self._developerKey, methodDesc, futureDesc)
 
     setattr(method, '__doc__', 'A description of how to use this function')
+    setattr(method, '__is_resource__', True)
     setattr(theclass, methodName, method)
 
   for methodName, methodDesc in resources.iteritems():
-    createMethod(Service, methodName, methodDesc, future[methodName])
+    createMethod(Service, methodName, methodDesc, future.get(methodName, {}))
   return Service()
 
 
-def createResource(http, baseUrl, model, resourceName, resourceDesc,
-    futureDesc):
+def createResource(http, baseUrl, model, resourceName, developerKey,
+                   resourceDesc, futureDesc):
 
   class Resource(object):
     """A class for interacting with a resource."""
@@ -148,9 +189,10 @@ def createResource(http, baseUrl, model, resourceName, resourceDesc,
       self._http = http
       self._baseUrl = baseUrl
       self._model = model
+      self._developerKey = developerKey
 
   def createMethod(theclass, methodName, methodDesc, futureDesc):
-    pathUrl = methodDesc['pathUrl']
+    pathUrl = methodDesc['restPath']
     pathUrl = re.sub(r'\{', r'{+', pathUrl)
     httpMethod = methodDesc['httpMethod']
 
@@ -163,18 +205,26 @@ def createResource(http, baseUrl, model, resourceName, resourceDesc,
     pattern_params = {}  # Parameters that must match a regex
     query_params = [] # Parameters that will be used in the query string
     path_params = {} # Parameters that will be used in the base URL
-    for arg, desc in methodDesc['parameters'].iteritems():
-      param = key2param(arg)
-      argmap[param] = arg
+    if 'parameters' in methodDesc:
+      for arg, desc in methodDesc['parameters'].iteritems():
+        param = key2param(arg)
+        argmap[param] = arg
 
-      if desc.get('pattern', ''):
-        pattern_params[param] = desc['pattern']
-      if desc.get('required', False):
-        required_params.append(param)
-      if desc.get('parameterType') == 'query':
-        query_params.append(param)
-      if desc.get('parameterType') == 'path':
-        path_params[param] = param
+        if desc.get('pattern', ''):
+          pattern_params[param] = desc['pattern']
+        if desc.get('required', False):
+          required_params.append(param)
+        if desc.get('restParameterType') == 'query':
+          query_params.append(param)
+        if desc.get('restParameterType') == 'path':
+          path_params[param] = param
+
+    for match in URITEMPLATE.finditer(pathUrl):
+      for namematch in VARNAME.finditer(match.group(0)):
+        name = key2param(namematch.group(0))
+        path_params[name] = name
+        if name in query_params:
+          query_params.remove(name)
 
     def method(self, **kwargs):
       for name in kwargs.iterkeys():
@@ -201,18 +251,25 @@ def createResource(http, baseUrl, model, resourceName, resourceDesc,
           actual_path_params[argmap[key]] = value
       body_value = kwargs.get('body', None)
 
+      if self._developerKey:
+        actual_query_params['key'] = self._developerKey
+
       headers = {}
       headers, params, query, body = self._model.request(headers,
           actual_path_params, actual_query_params, body_value)
 
+      # TODO(ade) This exists to fix a bug in V1 of the Buzz discovery document.
+      # Base URLs should not contain any path elements. If they do then urlparse.urljoin will strip them out
+      # This results in an incorrect URL which returns a 404
+      url_result = urlparse.urlsplit(self._baseUrl)
+      new_base_url = url_result.scheme + '://' + url_result.netloc
+
       expanded_url = uritemplate.expand(pathUrl, params)
-      url = urlparse.urljoin(self._baseUrl, expanded_url + query)
+      url = urlparse.urljoin(new_base_url, url_result.path + expanded_url + query)
 
       logging.info('URL being requested: %s' % url)
-      resp, content = self._http.request(
-          url, method=httpMethod, headers=headers, body=body)
-
-      return self._model.response(resp, content)
+      return HttpRequest(self._http, url, method=httpMethod, body=body,
+                         headers=headers, postproc=self._model.response)
 
     docs = ['A description of how to use this function\n\n']
     for arg in argmap.iterkeys():
@@ -243,8 +300,15 @@ def createResource(http, baseUrl, model, resourceName, resourceDesc,
         for key in methodDesc['location']:
           p = p[key]
         url = p
-      except KeyError:
+      except (KeyError, TypeError):
         return None
+
+      if self._developerKey:
+        parsed = list(urlparse.urlparse(url))
+        q = parse_qsl(parsed[4])
+        q.append(('key', self._developerKey))
+        parsed[4] = urllib.urlencode(q)
+        url = urlparse.urlunparse(parsed)
 
       headers = {}
       headers, params, query, body = self._model.request(headers, {}, {}, None)
@@ -252,18 +316,43 @@ def createResource(http, baseUrl, model, resourceName, resourceDesc,
       logging.info('URL being requested: %s' % url)
       resp, content = self._http.request(url, method='GET', headers=headers)
 
-      return self._model.response(resp, content)
+      return HttpRequest(self._http, url, method='GET',
+                         headers=headers, postproc=self._model.response)
 
     setattr(theclass, methodName, method)
 
   # Add basic methods to Resource
-  for methodName, methodDesc in resourceDesc['methods'].iteritems():
-    future = futureDesc['methods'].get(methodName, {})
-    createMethod(Resource, methodName, methodDesc, future)
+  if 'methods' in resourceDesc:
+    for methodName, methodDesc in resourceDesc['methods'].iteritems():
+      if futureDesc:
+        future = futureDesc['methods'].get(methodName, {})
+      else:
+        future = None
+      createMethod(Resource, methodName, methodDesc, future)
+
+  # Add in nested resources
+  if 'resources' in resourceDesc:
+    def createMethod(theclass, methodName, methodDesc, futureDesc):
+
+      def method(self):
+        return createResource(self._http, self._baseUrl, self._model,
+            methodName, self._developerKey, methodDesc, futureDesc)
+
+      setattr(method, '__doc__', 'A description of how to use this function')
+      setattr(method, '__is_resource__', True)
+      setattr(theclass, methodName, method)
+
+    for methodName, methodDesc in resourceDesc['resources'].iteritems():
+      if futureDesc and 'resources' in futureDesc:
+        future = futureDesc['resources'].get(methodName, {})
+      else:
+        future = {}
+      createMethod(Resource, methodName, methodDesc, future.get(methodName, {}))
 
   # Add <m>_next() methods to Resource
-  for methodName, methodDesc in futureDesc['methods'].iteritems():
-    if 'next' in methodDesc and methodName in resourceDesc['methods']:
-      createNextMethod(Resource, methodName + "_next", methodDesc['next'])
+  if futureDesc:
+    for methodName, methodDesc in futureDesc['methods'].iteritems():
+      if 'next' in methodDesc and methodName in resourceDesc['methods']:
+        createNextMethod(Resource, methodName + "_next", methodDesc['next'])
 
   return Resource()
--- main.py
+++ main.py
@@ -24,6 +24,7 @@ from google.appengine.api import users
 
 import buzz_gae_client
 import logging
+import oauth_handlers
 import os
 
 CONSUMER_KEY = 'anonymous'
@@ -32,66 +33,21 @@ CONSUMER_SECRET = 'anonymous'
 # Change this for your application.
 CALLBACK_URL = 'http://oauthisa4letterword.appspot.com/finish_dance'
 
-
-class UserToken(db.Model):
-    # The user_id is the key_name so we don't have to make it an explicit property
-    request_token_string = db.StringProperty()
-    access_token_string  = db.StringProperty()
-    email_address = db.StringProperty()
-
-    def get_request_token(self):
-        "Returns request token as a dictionary of tokens including oauth_token, oauth_token_secret and oauth_callback_confirmed."
-        return eval(self.request_token_string)
-
-    def set_access_token(self, access_token):
-        access_token_string = repr(access_token)
-        self.access_token_string = access_token_string
-
-    def get_access_token(self):
-        "Returns access token as a dictionary of tokens including consumer_key, consumer_secret, oauth_token and oauth_token_secret"
-        return eval(self.access_token_string)
-
-    @staticmethod
-    def create_user_token(request_token):
-        user = users.get_current_user()
-        user_id = user.user_id()
-        request_token_string = repr(request_token)
-
-        # TODO(ade) Support users who sign in to AppEngine with a federated identity aka OpenId
-        email = user.email()
-
-        logging.info('Creating user token: key_name: %s request_token_string: %s email_address: %s' % (user_id, request_token_string, email))
-        
-        return UserToken(key_name=user_id, request_token_string=request_token_string, access_token_string='', email_address=email)
-
-    @staticmethod
-    def get_current_user_token():
-        user = users.get_current_user()
-        user_token = UserToken.get_by_key_name(user.user_id())
-        return user_token
-
-    @staticmethod
-    def access_token_exists():
-        user = users.get_current_user()
-        user_token = UserToken.get_by_key_name(user.user_id())
-        logging.info('user_token: %s' % user_token)
-        return user_token and user_token.access_token_string
-
 class WelcomeHandler(webapp.RequestHandler):
     @login_required
     def get(self):
         logging.info("Request body %s" % self.request.body)
         template_values = {}
-        if UserToken.access_token_exists():
+        if oauth_handlers.UserToken.access_token_exists():
             template_values['access_token_exists'] = 'true'
         else:
             # Generate the request token
             client = buzz_gae_client.BuzzGaeClient(CONSUMER_KEY, CONSUMER_SECRET)
-            request_token = client.get_request_token(CALLBACK_URL)
+            request_token = client.get_request_token(self.request.host_url + '/finish_dance')
 
             # Create the request token and associate it with the current user
-            user_token = UserToken.create_user_token(request_token)
-            UserToken.put(user_token)
+            user_token = oauth_handlers.UserToken.create_user_token(request_token)
+            oauth_handlers.UserToken.put(user_token)
 
             authorisation_url = client.generate_authorisation_url(request_token)
             logging.info('Authorisation URL is: %s' % authorisation_url)  
@@ -100,47 +56,27 @@ class WelcomeHandler(webapp.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'welcome.html')
 	self.response.out.write(template.render(path, template_values))
 
-class TokenDeletionHandler(webapp.RequestHandler):
-    def post(self):
-        user_token = UserToken.get_current_user_token()
-        UserToken.delete(user_token)
-        self.redirect('/')
-
-class DanceFinishingHandler(webapp.RequestHandler):
-    def get(self):
-        logging.info("Request body %s" % self.request.body)
-        oauth_verifier = self.request.get('oauth_verifier')
-
-        client = buzz_gae_client.BuzzGaeClient(CONSUMER_KEY, CONSUMER_SECRET)
-        user_token = UserToken.get_current_user_token()
-        request_token = user_token.get_request_token()
-        access_token = client.upgrade_to_access_token(request_token, oauth_verifier)
-        logging.info('SUCCESS: Received access token.')
-
-        user_token.set_access_token(access_token)
-        UserToken.put(user_token)
-        logging.debug('Access token was: %s' % user_token.access_token_string)
-
-        self.redirect('/profile')
-
 class ProfileViewingHandler(webapp.RequestHandler):
-    @login_required
-    def get(self):
-        user_token = UserToken.get_current_user_token()
-        client = buzz_gae_client.BuzzGaeClient(CONSUMER_KEY, CONSUMER_SECRET)
-        api_client = client.build_api_client(user_token.get_access_token())
-        user_profile_data = api_client.people().get(userId='@me')
+  @login_required
+  def get(self):
+    # Users who don't have tokens should get sent to the front page
+    if not oauth_handlers.UserToken.access_token_exists():
+      self.redirect('/')
+      return
 
-        template_values = {}
-        template_values['user_profile_data'] = user_profile_data
-        template_values['access_token'] = user_token.access_token_string
-        path = os.path.join(os.path.dirname(__file__), 'profile.html')
-	self.response.out.write(template.render(path, template_values))
+    user_token = oauth_handlers.UserToken.get_current_user_token()      
+    buzz_wrapper = oauth_handlers.make_wrapper(user_token.email_address)
+    user_profile_data = buzz_wrapper.get_profile()
+
+    template_values = {'user_profile_data': user_profile_data, 'access_token': user_token.access_token_string}
+    path = os.path.join(os.path.dirname(__file__), 'profile.html')
+    self.response.out.write(template.render(path, template_values))
+
 
 application = webapp.WSGIApplication([
-('/', WelcomeHandler),
-('/delete_tokens', TokenDeletionHandler),
-('/finish_dance', DanceFinishingHandler),
+('/', oauth_handlers.DanceStartingHandler),
+('/delete_tokens', oauth_handlers.TokenDeletionHandler),
+('/finish_dance', oauth_handlers.DanceFinishingHandler),
 ('/profile', ProfileViewingHandler)],
   debug = True)
 
